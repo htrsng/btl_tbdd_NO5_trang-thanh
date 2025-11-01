7c5c5d3394284764d0d4e0342841cc84a74979e1
Kaitszo <thanhtb2005@gmail.com>
2025-11-01 04:07:27 +0000

    commit

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'screens/main_screen.dart';   // Import file mới
-import 'utils/app_theme.dart';      // Import theme
+import 'screens/main_screen.dart'; 
+import 'utils/app_theme.dart';      
 import 'l10n/app_localizations.dart';
 import 'providers/locale_provider.dart';
 import 'providers/theme_provider.dart';
