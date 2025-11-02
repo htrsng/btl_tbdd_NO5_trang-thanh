cecc84b15037c719c6ec21569fc443d8a505f0fa
htrsng <23010181@st.phenikaa-uni.edu.vn>
2025-11-02 20:58:30 +0700

    cap nhat

--- app/lib/providers/locale_provider.dart
+++ app/lib/providers/locale_provider.dart
@@ -1,3 +1,4 @@
+//Quản lý việc người dùng đang chọn ngôn ngữ "Anh" hay "Việt".
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
--- app/lib/providers/theme_provider.dart
+++ app/lib/providers/theme_provider.dart
@@ -1,3 +1,4 @@
+//Quản lý việc người dùng đang chọn chế độ "Sáng" hay "Tối".
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
